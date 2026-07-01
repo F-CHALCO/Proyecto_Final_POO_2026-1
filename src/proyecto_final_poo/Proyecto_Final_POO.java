e0fe641237f54bf31035f54b2853a52f0a62ca66
Your Name <chalcofabricio2109@gmail.com>
2026-07-01 00:10:47 -0500

    Este es mi segundo commit

--- src/proyecto_final_poo/Proyecto_Final_POO.java
+++ src/proyecto_final_poo/Proyecto_Final_POO.java
@@ -13,7 +13,8 @@ public class Proyecto_Final_POO {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Quiero keke");
+
     }
 
 }
