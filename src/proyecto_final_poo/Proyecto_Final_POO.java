44f8dffaa5a6f9a8787b1742c3ba5e033036c668
Your Name <chalcofabricio2109@gmail.com>
2026-07-01 00:02:14 -0500

    init project

---
+++ src/proyecto_final_poo/Proyecto_Final_POO.java
@@ -0,0 +1,19 @@
+
+package proyecto_final_poo;
+
+/**
+ *
+ * @author Charly Cimino
+ * Aprendé más Java en mi canal: https://www.youtube.com/c/CharlyCimino
+ * Encontrá más código en mi repo de GitHub: https://github.com/CharlyCimino
+ */
+public class Proyecto_Final_POO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
