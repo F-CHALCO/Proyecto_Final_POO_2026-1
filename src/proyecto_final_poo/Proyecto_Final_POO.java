e552bc5584ac33cd1db9cdc38912a57e740251d8
Your Name <chalcofabricio2109@gmail.com>
2026-07-02 17:44:24 -0500

    hola

--- src/proyecto_final_poo/Proyecto_Final_POO.java
+++ src/proyecto_final_poo/Proyecto_Final_POO.java
@@ -6,7 +6,7 @@ package proyecto_final_poo;
  * @author Charly Cimino
  * Aprendé más Java en mi canal: https://www.youtube.com/c/CharlyCimino
  * Encontrá más código en mi repo de GitHub: https://github.com/CharlyCimino
- */
+ */ 
 public class Proyecto_Final_POO {
 
     /**
@@ -14,7 +14,6 @@ public class Proyecto_Final_POO {
      */
     public static void main(String[] args) {
         System.out.println("Quiero keke");
-
     }
 
 }
