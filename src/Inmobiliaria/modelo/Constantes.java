438cadc664bad540162e8ed372a06fbed0367961
Your Name <chalcofabricio2109@gmail.com>
2026-07-02 20:32:42 -0500

    Definiendo las constantes (Diccionario)

---
+++ src/Inmobiliaria/modelo/Constantes.java
@@ -0,0 +1,36 @@
+
+package Inmobiliaria.modelo;
+
+/**
+ *
+ * @author Charly Cimino
+ * Aprendé más Java en mi canal: https://www.youtube.com/c/CharlyCimino
+ * Encontrá más código en mi repo de GitHub: https://github.com/CharlyCimino
+ */
+public class Constantes {
+    // roles de empleado
+    public static final String ROL_ASESOR = "ASESOR_VENTA";
+    public static final String ROL_ADMIN = "ADMINISTRADOR";
+    public static final String ROL_GERENTE = "GERENTE";
+
+    // estados de proyecto
+    public static final String PROY_EN_PLANOS = "EN_PLANOS";
+    public static final String PROY_EN_CONSTRUCCION = "EN_CONSTRUCCION";
+    public static final String PROY_TERMINADO = "TERMINADO";
+    public static final String PROY_ENTREGADO = "ENTREGADO";
+
+    // estados de departamento
+    public static final String DPTO_DISPONIBLE = "DISPONIBLE";
+    public static final String DPTO_RESERVADO = "RESERVADO";
+    public static final String DPTO_VENDIDO = "VENDIDO";
+
+    // tipos de departamento
+    public static final String TIPO_FLAT = "FLAT";
+    public static final String TIPO_DUPLEX = "DUPLEX";
+    public static final String TIPO_PENTHOUSE = "PENTHOUSE";
+
+    // modalidades de pago
+    public static final String PAGO_CONTADO = "CONTADO";
+    public static final String PAGO_BANCARIO = "FINANCIAMIENTO_BANCARIO";
+    public static final String PAGO_CUOTAS = "CUOTAS_DIRECTAS";
+}
