45161041c503adbda93072b8de17d3999b7c38c2
Your Name <chalcofabricio2109@gmail.com>
2026-07-02 22:36:21 -0500

    Clase Gerente extends Empleado

---
+++ src/Inmobiliaria/modelo/Gerente.java
@@ -0,0 +1,32 @@
+
+package Inmobiliaria.modelo;
+
+/**
+ *
+ * @author Charly Cimino
+ * Aprendé más Java en mi canal: https://www.youtube.com/c/CharlyCimino
+ * Encontrá más código en mi repo de GitHub: https://github.com/CharlyCimino
+ */
+public class Gerente extends Empleado {
+    private String areaResponsable;
+
+    public Gerente(String usuario, String contrasena, String rol, String dni, String nombres, String apellidos, String telefono, String correo) {
+        super(usuario, contrasena, rol, dni, nombres, apellidos, telefono, correo);
+    }
+
+    public String getAreaResponsable() {
+        return areaResponsable;
+    }
+
+    public void setAreaResponsable(String areaResponsable) {
+        this.areaResponsable = areaResponsable;
+    }
+
+    @Override
+    public String generarReporte() {
+        return "=== Reporte Gerente: " + getDatosCompletos() + " ===" +
+               "\nArea responsable: " + areaResponsable;
+    }
+    
+    
+}
