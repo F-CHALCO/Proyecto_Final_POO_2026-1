1e8e087ce26752049b3800a97379b6d307fb6bcb
Your Name <chalcofabricio2109@gmail.com>
2026-07-02 21:55:29 -0500

    Clase Empleado abstracta extends Persona

---
+++ src/Inmobiliaria/modelo/Empleado.java
@@ -0,0 +1,50 @@
+
+package Inmobiliaria.modelo;
+
+/**
+ *
+ * @author Charly Cimino
+ * Aprendé más Java en mi canal: https://www.youtube.com/c/CharlyCimino
+ * Encontrá más código en mi repo de GitHub: https://github.com/CharlyCimino
+ */
+public abstract class Empleado extends Persona {
+    private String usuario;
+    private String contrasena;
+    private String rol;
+
+    public Empleado(String usuario, String contrasena, String rol, String dni, String nombres, String apellidos, String telefono, String correo) {
+        super(dni, nombres, apellidos, telefono, correo);
+        this.usuario = usuario;
+        this.contrasena = contrasena;
+        this.rol = rol;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+    
+    public boolean login(String user, String pass){
+        return this.usuario.equals(user) && this.contrasena.equals(pass);
+    }
+    
+}
