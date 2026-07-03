39a00b0ad3269a238340bdb25995be375e89f072
Your Name <chalcofabricio2109@gmail.com>
2026-07-02 21:30:45 -0500

    Clase Persona

---
+++ src/Inmobiliaria/modelo/Persona.java
@@ -0,0 +1,79 @@
+
+package Inmobiliaria.modelo;
+
+/**
+ *
+ * @author Charly Cimino
+ * Aprendé más Java en mi canal: https://www.youtube.com/c/CharlyCimino
+ * Encontrá más código en mi repo de GitHub: https://github.com/CharlyCimino
+ */
+public abstract class Persona {
+    private String dni;
+    private String nombres;
+    private String apellidos;
+    private String telefono;
+    private String correo;
+
+    public Persona(String dni, String nombres, String apellidos, String telefono, String correo) {
+        this.dni = dni;
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+        this.telefono = telefono;
+        this.correo = correo;
+    }
+    
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+    
+    public abstract String generarReporte();
+    
+    public String getDatosCompletos(){
+        return nombres + " " + apellidos +"(DNI: "+ dni + ")";
+    }
+
+    @Override
+    public String toString() {
+        return getDatosCompletos();
+    }
+    
+    
+    
+}
